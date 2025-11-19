931d2eb99e4254ecbdc0ef14fe0be7e307960c13
Hafis-Mohamed <hafismohamed2004@gmail.com>
2025-11-19 15:51:00 +0530

    csv

--- PYTHON/dict_csv.py
+++ PYTHON/dict_csv.py
@@ -1,1 +1,23 @@
-dict=[{"Name":"hafis","Age":"21","Country":"india"}]
+
+import csv
+
+# A list of dictionaries
+mydict = [
+    {"Name": "Harry", "Age": "19"},
+    {"Name": "Ron", "Age": "13"}, 
+    {"Name": "Hermione", "Age": "78"}
+]
+
+# Write the csv file
+with open("new.csv", "w") as file:
+    writer = csv.DictWriter(file, fieldnames=["Name", "Age"])
+    writer.writeheader()
+    writer.writerows(mydict)
+    
+    
+# Read the file
+with open("new.csv", "r") as csvfile:
+    file = csv.DictReader(csvfile)
+    for row in file:
+        print(row)
+    
